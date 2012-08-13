18dcdf570f1d130faa84bc25664ae08e9a8fb686
natsu <mouatchy@192.168.1.43>
2012-08-13 11:16:12 +0200

    Passive skills And update Hero class with new classes

--- src/com/myself/game/characters/Hero.java
+++ src/com/myself/game/characters/Hero.java
@@ -1,7 +1,11 @@
 package com.myself.game.characters;
 
+import com.myself.game.skills.MagicalSkills;
+import com.myself.game.skills.PassiveSkills;
+
 public class Hero {
 
 	private String name;
-	private String[] skills;
+	private PassiveSkills[] passiveSkills;
+	private MagicalSkills[] magicalSkills;
 }
